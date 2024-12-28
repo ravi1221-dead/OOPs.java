3f53b28066b4cd04f588097710588e94c1e3997d
THONTLA RAVI CHANDRA REDDY <ravichinna2001msd@gmail.com>
2024-12-28 19:50:16 +0530

    Auto commit and push

--- Polymorphism/MethodOverLoading.java
+++ Polymorphism/MethodOverLoading.java
@@ -1,4 +1,24 @@
 package Polymorphism;
 
+class Printer {
+    public void print(String message) {
+        System.out.println("String message: " + message);
+    }
+
+    public void print(int number) {
+        System.out.println("Integer number: " + number);
+    }
+
+    public void print(double value) {
+        System.out.println("Double value: " + value);
+    }
+}
+
 public class MethodOverLoading {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        printer.print("Hello Ravi"); // Calls print(String message)
+        printer.print(18);           // Calls print(int number)
+        printer.print(0.7);          // Calls print(double value)
+    }
 }
